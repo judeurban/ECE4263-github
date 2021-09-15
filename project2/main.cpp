9e8e83169de30a91bc4b292899873d41e915f73a
judeurban <jude_urban@utulsa.edu>
2021-09-15 08:39:28 -0500

    Class
    Created class Node

--- project2/main.cpp
+++ project2/main.cpp
@@ -1,7 +1,47 @@
+// huffman compression algorithm in c++
+//jude urban
+//14 september 2021
+
 #include <iostream>
 
 using namespace std;
 
+class Node
+{
+private:
+    /* data */
+public:
+    Node * leftchild;
+    Node * rightchild;
+    string value;
+    Node(/* args */);
+
+    // ~Node();
+};
+
+//constuctor 1
+Node::Node(/* args */)
+{
+}
+
+
+//constructor 2
+// Node::~Node()
+// {
+// }
+
+
 int main() {
+    Node rootnode;
+    Node leftchildnode;
+    Node rightchildnode;
+    rootnode.leftchild = &leftchildnode;
+    rootnode.rightchild = &rightchildnode;
+
+    cout << "leftchild node is at" << &leftchildnode << endl;
+    cout << "rightchild node is at" << &rightchildnode << endl;
+    cout << "rootnode left child is at" << rootnode.leftchild << endl;
+    cout << "rootnode right child is at" << rootnode.rightchild << endl;
+
     return 0;
 }
