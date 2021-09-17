265426e385664737a7a22a44db92e1e0bc248dc8
judeurban <jude_urban@utulsa.edu>
2021-09-16 20:42:54 -0500

    I am an idiot

---
+++ project2/project2_clion/cmake-build-debug/CMakeFiles/project2_clion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project2_clion.dir/main.cpp.o"
+  "project2_clion"
+  "project2_clion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project2_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
