441a3b7a025c60194aa8625bd07bf513e34ea147
judeurban <jude_urban@utulsa.edu>
2021-10-05 12:42:54 -0500

    project 1 address fixed, added library files .c and .h

--- STM_workspace/project1/Core/Inc/4263_MPU6050.h
+++ STM_workspace/project1/Core/Inc/4263_MPU6050.h
@@ -1,5 +1,25 @@
-#define MPU6050_I2CADDR_DEFAULT                                                \
-  0x68                         ///< MPU6050 default i2c address w/ AD0 high
+/*  
+
+device registers 
+
+hex     decimal     register
+
+3B      59          ACCEL_XOUT[15:8]
+3C      60          ACCEL_XOUT[7:0]
+3D      61          ACCEL_YOUT[15:8]
+3E      62          ACCEL_YOUT[7:0]
+3F      63          ACCEL_ZOUT[15:8]
+40      64          ACCEL_ZOUT[7:0]
+
+source: https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf
+
+*/
+
+// #define MPU6050_I2CADDR_DEFAULT                                                \
+//   0x68                         ///< MPU6050 default i2c address w/ AD0 high
+
+#define MPU6050_I2CADDR_DEFAULT 0xD0                                         \
+
 #define MPU6050_DEVICE_ID 0x68 ///< The correct MPU6050_WHO_AM_I value
 
 #define MPU6050_SELF_TEST_X                                                    \
@@ -20,7 +40,7 @@
 #define MPU6050_ACCEL_CONFIG                                                   \
   0x1C ///< Accelerometer specific configration register
 #define MPU6050_INT_PIN_CONFIG 0x37    ///< Interrupt pin configuration register
-#define MPU6050_WHO_AM_I 0x75          ///< Divice ID register
+#define MPU6050_WHO_AM_I (0x75)          ///< Divice ID register
 #define MPU6050_SIGNAL_PATH_RESET 0x68 ///< Signal path reset register
 #define MPU6050_USER_CTRL 0x6A         ///< FIFO and I2C Master control register
 #define MPU6050_PWR_MGMT_1 0x6B        ///< Primary power/sleep control register
@@ -29,30 +49,17 @@
 #define MPU6050_TEMP_L 0x42     ///< Temperature data low byte register
 #define MPU6050_ACCEL_OUT 0x3B  ///< base address for sensor data reads
 
-/*
+int16_t X_RAW_accel;
+int16_t Y_RAW_accel;
+int16_t Z_RAW_accel;
 
-hex     decimal     register
-
-3B      59          ACCEL_XOUT[15:8]
-3C      60          ACCEL_XOUT[7:0]
-3D      61          ACCEL_YOUT[15:8]
-3E      62          ACCEL_YOUT[7:0]
-3F      63          ACCEL_ZOUT[15:8]
-40      64          ACCEL_ZOUT[7:0]
-
-source: https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf
-
-*/
-
-int16_t X_RAW_accel = 0;
-int16_t Y_RAW_accel = 0;
-int16_t Z_RAW_accel = 0;
-
-int16_t X_RAW_gyro = 0;
-int16_t Y_RAW_gyro = 0;
-int16_t Z_RAW_gyro = 0;
+int16_t X_RAW_gyro;
+int16_t Y_RAW_gyro;
+int16_t Z_RAW_gyro;
 
 float Ax, Ay, Az, Gx, Gy, Gz;
 
 void MPU6050_init(I2C_HandleTypeDef *);
 void MPU6050_Read_Accel(I2C_HandleTypeDef *);
+void MPU6050_Read_Gyro(I2C_HandleTypeDef *);
+
--- STM_workspace/project1/Core/Src/main.c
+++ STM_workspace/project1/Core/Src/main.c
@@ -19,7 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "jmuMPU6050.h"
+#include "4263_MPU6050.h"
 
 //#include "Adafruit_MPU6050.h"
 
@@ -92,7 +92,9 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+
   MX_I2C4_Init();
+  
   /* USER CODE BEGIN 2 */
 
   MPU6050_init(&hi2c4);
@@ -220,84 +222,6 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
-void MPU6050_init(I2C_HandleTypeDef * hi2c)
-{
-  uint8_t check;
-  uint8_t data;
-
-  /*  
-    check if the device is ready  
-    a return of 0x68 implies a healthy connection
-  */
-  
-  // HAL_I2C_Mem_Read(hi2c, MPU6050_I2CADDR_DEFAULT, 0x68, 1, &check, 1, 1000);    //returns 0x20 or 0d32
-  HAL_I2C_Mem_Read(hi2c, MPU6050_I2CADDR_DEFAULT, MPU6050_WHO_AM_I, 1, &check, 1, 1000);
-  // if(check == (0x68))
-  if(check == (0x20))
-  {
-		// power management register 0X6B we should write all 0's to wake the sensor up
-    data = 0x00;
-    HAL_I2C_Mem_Write(hi2c, MPU6050_I2CADDR_DEFAULT, MPU6050_PWR_MGMT_1, 1, &data, 1, 1000);
-  
-    // Set DATA RATE of 1KHz by writing SMPLRT_DIV register
-		data = 0x07;
-		HAL_I2C_Mem_Write(hi2c, MPU6050_I2CADDR_DEFAULT, MPU6050_SMPLRT_DIV, 1, &data, 1, 1000);
-  
-		// Set accelerometer configuration in ACCEL_CONFIG Register
-		// XA_ST=0,YA_ST=0,ZA_ST=0, FS_SEL=0 -> � 2g
-		data = 0x00;
-		HAL_I2C_Mem_Write(hi2c, MPU6050_I2CADDR_DEFAULT, MPU6050_ACCEL_CONFIG, 1, &data, 1, 1000);
-
-    // Set Gyroscopic configuration in GYRO_CONFIG Register
-		// XG_ST=0,YG_ST=0,ZG_ST=0, FS_SEL=0 -> � 250 �/s
-		data = 0x00;
-		HAL_I2C_Mem_Write(hi2c, MPU6050_I2CADDR_DEFAULT, MPU6050_GYRO_CONFIG, 1, &data, 1, 1000);
-  
-  }
-}
-
-//read accelerometer data from the MPU6050. pass the I2C bus!
-void MPU6050_Read_Accel(I2C_HandleTypeDef * hi2c)
-{
-  uint8_t rcvd_data[6];
-
-  //read all six byes of data from the accelerometer
-  //error!!
-  HAL_I2C_Mem_Read(hi2c, MPU6050_I2CADDR_DEFAULT, MPU6050_ACCEL_OUT, 1, &rcvd_data, 6, 1000);
-
-  //                          MSB(yte)           LSB(yte)
-  X_RAW_accel = (uint16_t)(rcvd_data[0] << 8 | rcvd_data[1]);
-  Y_RAW_accel = (uint16_t)(rcvd_data[2] << 8 | rcvd_data[3]);
-  Z_RAW_accel = (uint16_t)(rcvd_data[4] << 8 | rcvd_data[5]);
-
-  /*  16384.0 is device constant  */
-  Ax = X_RAW_accel / 16384.0;
-  Ay = Y_RAW_accel / 16384.0;
-  Az = Z_RAW_accel / 16384.0;
-
-}
-
-//read gyro data from the MPU6050. pass the I2C bus!
-void MPU6050_Read_Gyro(I2C_HandleTypeDef * hi2c)
-{
-  uint8_t rcvd_data[6];
-
-  //read all six byes of data from the gyro
-  //error!!
-  HAL_I2C_Mem_Read(hi2c, MPU6050_I2CADDR_DEFAULT, MPU6050_GYRO_OUT, 1, &rcvd_data, 6, 1000);
-
-  X_RAW_gyro = (uint16_t)(rcvd_data[0] << 8 | rcvd_data[1]);
-  Y_RAW_gyro = (uint16_t)(rcvd_data[2] << 8 | rcvd_data[3]);
-  Z_RAW_gyro = (uint16_t)(rcvd_data[4] << 8 | rcvd_data[5]);
-
-  /*  131.0 is device constant  */
-  Gx = X_RAW_gyro / 131.0;
-  Gy = Y_RAW_gyro / 131.0;
-  Gz = Z_RAW_gyro / 131.0;
-
-}
-
 
 /* USER CODE END 4 */
 
