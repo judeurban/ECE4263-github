2b89a35ebb4fca23cb2b55f3916b2ad8e64e9d5c
judeurban <jude_urban@utulsa.edu>
2021-12-15 10:25:14 -0600

    modified server

--- STM_workspace/ETHERNET/Server/server.py
+++ STM_workspace/ETHERNET/Server/server.py
@@ -1,13 +1,14 @@
 import time
 import socket
 
-hostIP     = "192.168.2.95"
-hostPort   = 20001
+hostIP     = "129.244.42.177"
+hostPort   = 12345
 bufferSize  = 1024
 
 validID = ("6013CA34", "8023BC37", "8B9BB54C", "5F9BA54F", "595BA47D")
 
 def createSocket():
+
     global UDPServerSocket
 
     # Create a datagram socket
@@ -18,60 +19,7 @@ def createSocket():
 
     print("UDP server up and listening")
 
-def listenForRequests():
-
-    try:
-        # Listen for incoming datagrams
-        while True:
-
-            clientMessage, clientAdress = UDPServerSocket.recvfrom(bufferSize)
-            
-            clientMessage = clientMessage.decode('utf-8')
-
-            if clientMessage in validID:
-                access = True
-                print("access granted")
-            else:
-                access = False
-                print("access denied")
-
-            # read client message, go get log....
-
-            # filter
-            # if clientAdress == "192.168.2.90":
-
-            # Sending a reply to client
-            # if user has access, send TRUE
-
-            sendStr = "{},{}".format(clientMessage, str(access)).encode('utf-8')
-            UDPServerSocket.sendto(sendStr, clientAdress)
-            
-    except KeyboardInterrupt:
-        pass
-
-    UDPServerSocket.close()
-
-# first time handshake
-def initialize():
-
-    serverReady = False
-
-    while not serverReady:
-
-        clientMessage, clientAdress = UDPServerSocket.recvfrom(bufferSize)
-        
-        print("Client found at", clientAdress)
-
-        clientMessage = clientMessage.decode('utf-8')
-
-        print(clientMessage, end='')
-
-        if clientMessage == "stmF746ZG_ready":
-            # send confirmation
-            UDPServerSocket.sendto("server_ready".encode('utf-8'), clientAdress)
-            serverReady = True
-
-def debugWhileAlwaysOn():
+def AlwaysOn():
     try:
         # Listen for incoming datagrams
         while True:
@@ -79,37 +27,58 @@ def debugWhileAlwaysOn():
             clientMessage, clientAdress = UDPServerSocket.recvfrom(bufferSize)
             clientMessage = clientMessage.decode('utf-8')
 
+            print("\nclient: {}\treceived message: '{}'".format(clientAdress, clientMessage))
+
+            # handshake between client and server
             if clientMessage == "stmF746ZG_ready":
                 # send confirmation
-                UDPServerSocket.sendto("server_ready".encode('utf-8'), clientAdress)
+                sendStr = "server_ready"
+                UDPServerSocket.sendto(sendStr.encode('utf-8'), clientAdress)
+                print("client: {}\tsent message: '{}'".format(clientAdress, sendStr))
                 continue
-
-            if clientMessage in validID:
-                access = True
-                print("access granted")
+            # requesting access
             else:
-                access = False
-                print("access denied")
+                if clientMessage in validID:
+                    access = True
+                    print("access granted")
+                else:
+                    access = False
+                    print("access denied")
 
-            # read client message, go get log....
+                sendStr = str(access)
 
-            # filter
-            # if clientAdress == "192.168.2.90":
-
-            # Sending a reply to client
-            # if user has access, send TRUE
-
-            sendStr = "{},{}".format(clientMessage, str(access)).encode('utf-8')
-            UDPServerSocket.sendto(sendStr, clientAdress)
+                UDPServerSocket.sendto(sendStr.encode('utf-8'), clientAdress)
+                print("client: {}\tsent message: '{}'\n".format(clientAdress, sendStr))
             
     except KeyboardInterrupt:
-        pass
+        UDPServerSocket.close()
+        exit()
 
-    UDPServerSocket.close()
 
         
-
+print("\n")
 createSocket()
-# initialize()
-# listenForRequests()
-debugWhileAlwaysOn()
+AlwaysOn()
+'''
+
+    UDP server up and listening
+    client: ('129.244.42.104', 55555)       received message: 'stmF746ZG_ready'
+    client: ('129.244.42.104', 55555)       sent message: 'server_ready'
+
+
+    client: ('129.244.42.104', 55555)       received message: '8B8BB54C'
+    access denied
+    client: ('129.244.42.104', 55555)       sent message: 'False'
+
+    client: ('129.244.42.104', 55555)       received message: '3F9BA57F'
+    access denied
+    client: ('129.244.42.104', 55555)       sent message: 'False'
+
+    client: ('129.244.42.104', 55555)       received message: '595BA47D'
+    access granted
+    client: ('129.244.42.104', 55555)       sent message: 'True'
+
+
+'''
+
+
